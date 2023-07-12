dc30cedb7eb768b815ed3130a770ba28d38fb9b7
macius06Xd <wojteknecel@gmail.com>
2023-07-12 21:05:32 +0200

    tests

---
+++ PriorityQueue.py
@@ -0,0 +1,25 @@
+class PriorityQueue:
+    def __init__(self):
+
+        self.items = []
+
+    def is_empty(self):
+        # Returns True if the priority queue is empty, False otherwise
+        pass
+
+    def size(self):
+        # Returns the number of elements in the priority queue
+        pass
+
+    def enqueue(self, item, priority):
+        # Inserts an item into the priority queue based on its priority
+        item_to_insert = (item,priority)
+        pass
+
+    def dequeue(self):
+        # Removes and returns the item with the highest priority from the queue
+        pass
+
+    def peek(self):
+        # Returns the item with the highest priority without removing it
+        pass
---
+++ QueueTest.py
@@ -0,0 +1,46 @@
+import unittest
+
+class PriorityQueueTests(unittest.TestCase):
+    def setUp(self):
+        self.queue = PriorityQueue()
+
+    def test_is_empty(self):
+        self.assertTrue(self.queue.is_empty())
+        self.queue.enqueue('A', 1)
+        self.assertFalse(self.queue.is_empty())
+
+    def test_size(self):
+        self.assertEqual(0, self.queue.size())
+        self.queue.enqueue('A', 1)
+        self.assertEqual(1, self.queue.size())
+        self.queue.enqueue('B', 2)
+        self.assertEqual(2, self.queue.size())
+        self.queue.dequeue()
+        self.assertEqual(1, self.queue.size())
+
+    def test_enqueue(self):
+        self.queue.enqueue('A', 1)
+        self.assertEqual('A', self.queue.peek())
+        self.queue.enqueue('B', 3)
+        self.assertEqual('A', self.queue.peek())
+        self.queue.enqueue('C', 2)
+        self.assertEqual('A', self.queue.peek())
+
+    def test_dequeue(self):
+        self.queue.enqueue('A', 1)
+        self.queue.enqueue('B', 3)
+        self.queue.enqueue('C', 2)
+        self.assertEqual('B', self.queue.dequeue())
+        self.assertEqual('C', self.queue.dequeue())
+        self.assertEqual('A', self.queue.dequeue())
+
+    def test_peek(self):
+        self.queue.enqueue('A', 1)
+        self.assertEqual('A', self.queue.peek())
+        self.queue.enqueue('B', 3)
+        self.assertEqual('A', self.queue.peek())
+        self.queue.enqueue('C', 2)
+        self.assertEqual('A', self.queue.peek())
+
+if __name__ == '__main__':
+    unittest.main()
